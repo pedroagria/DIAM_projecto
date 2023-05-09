8cd003ad5e78344b1d282b88b6fbf6c72f7b23a5
pmaaa2-iscte <pmaaa2@iscte-iul.pt>
2023-05-09 09:21:14 +0100

    Adicionados os templates aos urls e views

--- boardgamecafe/urls.py
+++ boardgamecafe/urls.py
@@ -5,4 +5,9 @@ app_name = 'boardgamecafe'
 urlpatterns = [
     path('', views.index, name='index'),
     path('tempband', views.tempband, name='tempband'),
+    path('tempcafe', views.tempcafe, name='tempcafe'),
+    path('tempfoodblog', views.tempfoodblog, name='tempfoodblog'),
+    path('tempgourmetcatering', views.tempgourmetcatering, name='tempgourmetcatering'),
+    path('tempstartpage', views.tempstartpage, name='tempstartpage'),
+    path('tempstartup', views.tempstartup, name='tempstartup'),
 ]
--- boardgamecafe/views.py
+++ boardgamecafe/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Esta é a página de entrada da app boardgamecafe </h1>")
+    return render(request, 'boardgamecafe/index.html')
+    #return HttpResponse("<h1>Esta é a página de entrada da app boardgamecafe </h1>")
 
 def tempband(request):
     return render(request, 'boardgamecafe/tempband.html')
 
-def tempband(request):
-    return render(request, 'boardgamecafe/tempband.html')
+def tempcafe(request):
+    return render(request, 'boardgamecafe/tempcafe.html')
 
-def tempband(request):
-    return render(request, 'boardgamecafe/tempband.html')
+def tempfoodblog(request):
+    return render(request, 'boardgamecafe/tempfoodblog.html')
+
+def tempgourmetcatering(request):
+    return render(request, 'boardgamecafe/tempgourmetcatering.html')
+
+def tempstartpage(request):
+    return render(request, 'boardgamecafe/tempstartpage.html')
+
+def tempstartup(request):
+    return render(request, 'boardgamecafe/tempstartup.html')
